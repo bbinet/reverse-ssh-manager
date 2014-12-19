b459d0079ebe888e770a653228ae7c042b4e067d
Bruno Binet <bruno.binet@gmail.com>
2014-12-19 16:58:21 +0100

    git add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='reverse-ssh-server',
+    version='0.1',
+    description='reverse-ssh-server allows to manage reverse ssh connections '
+                'through a web application',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    author='Bruno Binet',
+    author_email='bruno.binet@gmail.com',
+    keywords=['ssh', 'tunnel', 'reverse', 'manage', 'web', 'application'],
+    url='https://github.com/bbinet/reverse-ssh-server',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        ],
+    entry_points={
+        'console_scripts': ['reverse-ssh-server=rss:run']
+        },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'bottle',
+        'psutil',
+    ],
+)
