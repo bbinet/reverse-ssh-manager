19b8408e282db21887ef8f4b75eff826665fe164
Bruno Binet <bruno.binet@gmail.com>
2014-12-22 18:28:16 +0100

    update uuid state on the server when switch is toggled

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,7 @@ app.controller('MainCtrl', ['$scope', '$http',
 
     $scope.displayedCollection = [];
 
+    var watchers = [];
     var error_cb = function() {
       console.log('catch error from server');
     };
@@ -14,11 +15,25 @@ app.controller('MainCtrl', ['$scope', '$http',
         url: '../uuids',
         method: 'GET'
       }).success(function(resp) {
-        if (! resp.success) {
+        if (!resp.success) {
           error_cb();
           return;
         }
+        angular.forEach(watchers, function(unwatch, idx) {
+          // unregister old watchers
+          unwatch();
+        });
         $scope.internalCollection = resp.db;
+        angular.forEach($scope.internalCollection, function(item, idx) {
+          watchers.push(
+            $scope.$watch(
+              'displayedCollection[' + idx +'].active', function(active) {
+                $http.post('../uuid/' + item.uuid, {active: active})
+                  .success(function(resp) {
+                    $scope.displayedCollection[idx] = resp;
+                  });
+              }));
+        });
       }).error(error_cb);
     };
 
