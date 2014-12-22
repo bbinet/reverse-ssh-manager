0cfa9074fc79e232533e43ae9f604cc99d3934af
Bruno Binet <bruno.binet@gmail.com>
2014-12-22 11:02:13 +0100

    add ssh connection status to the json response

--- rss/server.py
+++ rss/server.py
@@ -2,6 +2,7 @@ import time
 import sys
 import logging
 
+import psutil
 import bottle
 
 
@@ -24,6 +25,20 @@ app.config.load_dict({
     })
 
 db = {}
+port_counter = app.config['rss.port-start']
+
+
+def netstat(port):
+    status = {
+        'listen': False,
+        'established': [],
+        }
+    for c in psutil.net_connections(kind='tcp'):
+        if port in c.laddr and c.status == 'LISTEN':
+            status['listen'] = True
+        if port in c.raddr and c.status == 'ESTABLISHED':
+            status['established'].append(c.pid)
+    return status
 
 
 @bottle.get('/')
@@ -36,17 +51,30 @@ def index():
 
 
 @bottle.get('/<uuid>')
-def hook(uuid):
+def status(uuid):
+    global port_counter
     if not uuid in db:
-        db[uuid] = {}
+        db[uuid] = {
+            'uuid': uuid,
+            'name': bottle.request.query.get('name'),
+            'port': port_counter,
+            'active': False,
+            'listen': False,
+            'established': [],
+            }
+        port_counter += 1
+    db[uuid]['name'] = bottle.request.query.get('name')
     db[uuid]['time'] = int(time.time())
+    db[uuid].update(netstat(db[uuid]['port']))
     return db[uuid]
 
 
 def run():
+    global port_counter
     if len(sys.argv) > 1:
         for cfg in sys.argv[1:]:
             app.config.load_config(cfg)
+    port_counter = app.config['rss.port-start']
     if app.config['debug']:
         log.setLevel(logging.DEBUG)
         bottle.debug(True)
