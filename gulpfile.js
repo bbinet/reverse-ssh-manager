748520e7e8836aa0fb0a1278b47bf2c08d3d8403
Bruno Binet <bruno.binet@gmail.com>
2014-12-23 10:51:12 +0100

    add gulpfile.js to build the project

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+
+gulp.task('lint', function() {
+  gulp.src(['static/js/*.js'])
+    .pipe(jshint('.jshintrc'))
+      .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('usemin', function() {
+  gulp.src('static/index.html')
+    .pipe(usemin({
+      assetsDir: 'static',
+      css: [minifyCss(),rev()],
+      js: [uglify(), rev()]
+      //js: [uglify({mangle: false}), rev()]
+    }))
+    .pipe(gulp.dest('rss/static'));
+});
+
+gulp.task('img', function() {
+  gulp.src('static/img/*')
+    .pipe(gulp.dest('rss/static/img'));
+});
+
+gulp.task('default', ['usemin', 'img', 'lint']);
